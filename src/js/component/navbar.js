46cf60f63669c54b745298337c748b07979c1244
KrisN9 <kristinank992@gmail.com>
2023-04-19 19:03:55 +0000

    proyecto pendiente

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav class="navbar navbar-expand-lg bg-light">
-			<div class="container-fluid">
-				<a class="navbar-brand" href="#">Poner logo StarWars</a>
+		<nav className="navbar navbar-expand-lg bg-light">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">Logo Star Wars</a>
 			</div>
-			<div class="btn-group px-auto" role="group">
-				<button type="button" class="btn btn-primary btn-lg dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+			<div className="btn-group px-auto" role="group">
+				<button type="button" className="btn btn-primary btn-lg dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
 				Favorites
 				</button>
-				<ul class="dropdown-menu">
-					<li><a class="dropdown-item" href="#"></a></li>
+				<ul className="dropdown-menu">
+					<li><a className="dropdown-item" href="#"></a></li>
 				</ul>
 			</div>
 		</nav>)
